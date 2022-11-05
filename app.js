206a700dd67d57942c8ccd0c9df4f75dfb5c7ee2
vvskv <vvskvorcov@yandex.ru>
2022-11-05 13:48:35 +0300

    Условия IF

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+const age = 15;
+const gender = 'wOman';
+if(age > 18) {
+    console.log("You are really old");
+}
+if (gender.toLocaleLowerCase() === 'man') {
+    console.log('On the left');
+}
+if (gender.toLocaleLowerCase() === 'woman') {
+    console.log('On the right');
+}
